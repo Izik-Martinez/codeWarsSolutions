5ae9063c39e96f4dc64cc0cabc1b2d6190b151f2
Izik-Martinez <izikma17@yahoo.com>
2022-05-28 19:02:29 -0400

    How many pages in a book

---
+++ solution020.js
@@ -0,0 +1,9 @@
+function amountOfPages(summary) {
+    let counter = 1;
+    let string = "";
+    while (string.length < summary) {
+        string += counter;
+        counter++
+    }
+    return counter - 1;
+}
