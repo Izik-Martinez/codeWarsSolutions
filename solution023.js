462f94bb5e4d0dcb6fab36cfaa4757425e534b7a
Izik-Martinez <izikma17@yahoo.com>
2022-05-31 14:25:00 -0400

    Remove first and last char of string in JS and Lua

---
+++ solution023.js
@@ -0,0 +1,12 @@
+function removeChar(str){
+    //You got this!
+    return str.slice(1, -1);
+   };
+  
+// ----Answer in Lua----
+// local kata={}
+// function kata.remove_chars(s)
+// return string.sub(s, 2, -2)
+// end
+
+// return kata   
