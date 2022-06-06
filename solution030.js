93bcf687d9d1bd0b2adf08dbacd27cd8c3367e30
Izik-Martinez <izikma17@yahoo.com>
2022-06-06 19:45:18 -0400

    find the vowels

---
+++ solution030.js
@@ -0,0 +1,9 @@
+function vowelIndices(word) {
+    let arr = [];
+    for(let i = 0; i < word.length; i++) {
+      if(/[aeioyu]/i.test(word[i])) {
+        arr.push(i+1);
+      }
+    }
+    return arr;
+  }
